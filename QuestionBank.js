3c6ea4c018afbbfbf3c03cad85906249e7730325
Suhavi Kaur <101411563+Suhavikaur@users.noreply.github.com>
2024-09-24 19:09:29 +0530

    Add files via upload

---
+++ QuestionBank.js
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+    {
+        id: 1,
+        question: "How many sides are in a triangle?",
+        options: ["1", "2", "3", "4"],
+        answer: "3",
+    },
+    {
+        id: 2,
+        question: "What is the capital of Punjab?",
+        options: ["Patiala", "Ludhiana", "Amritsar", "Chandigarh"],
+        answer: "Chandigarh",
+    },
+    {
+        id: 3,
+        question: "What is the capital of India?",
+        options: ["Delhi", "Mumbai", "Kolkata", "Chennai"],
+        answer: "Delhi"
+    },
+    {
+        id: 4,
+        question: "How many sides are there in a rectangle?",
+        options: ["1", "2", "3", "4"],
+        answer: "4"
+    },
+    {
+        id: 5,
+        question: "What is the value of pi",
+        options: ["3", "2", "3.14", "4"],
+        answer: "3.14"
+    },
+]
+
+export default qBank;
